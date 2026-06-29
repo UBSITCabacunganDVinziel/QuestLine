399b60dcb266869fe25e8098a3985d59d6d4f954
barillo_k <barillo_k@f112.ub.lan>
2026-06-29 11:01:45 +0800

    Updated quest-form.html

--- src/app/quest-form/quest-form.ts
+++ src/app/quest-form/quest-form.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { QuestService } from '../quest-sevice';
 import { CHORE_LIST, REWARD_TIERS } from '../../quest.model';
 
-
 @Component({
   selector: 'app-quest-form',
   standalone: true,
