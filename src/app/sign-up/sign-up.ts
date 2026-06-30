086e62feab2ed9b8fd046848cb269b0c79963e9d
barillo_k <barillo_k@f112.ub.lan>
2026-06-30 10:54:12 +0800

    Added sign-up.ts

--- src/app/sign-up/sign-up.ts
+++ src/app/sign-up/sign-up.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { QuestService } from '../quest-sevice';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
-  templateUrl: './sign-up.html',
-  styleUrl: './sign-up.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './sign-up.html'
 })
-export class SignUp {}
+export class SignUp {
+  private questService = inject(QuestService);
+
+  public username = '';
+  public email = '';
+  public password = '';
+
+  onSwitchToSignIn = output<void>();
+
+  onSignUp() {
+    if (!this.username || !this.email || !this.password) return;
+    this.questService.signUpPlayer({
+      username: this.username,
+      email: this.email,
+      password: this.password
+    });
+    this.onSwitchToSignIn.emit();
+  }
+}
