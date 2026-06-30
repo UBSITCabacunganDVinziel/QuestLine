732d575b5295398f568992a5dc4cdd281618073e
barillo_k <barillo_k@f112.ub.lan>
2026-06-30 11:14:22 +0800

    Updated app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { QuestForm } from './quest-form/quest-form';
 import { QuestList } from './quest-list/quest-list';
+import { SignIn } from './sign-in/sign-in';
+import { SignUp } from './sign-up/sign-up';
+import { QuestService } from './quest-sevice';
 
 @Component({
   selector: 'app-root',
-  imports: [QuestForm, QuestList],
+  standalone: true,
+  imports: [CommonModule, QuestForm, QuestList, SignIn, SignUp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('questline-app');
+  public questService = inject(QuestService);
+  
+  public currentAuthSubPage = signal<'signin' | 'signup'>('signin');
+
+  navigateToPage(page: 'signin' | 'signup') {
+    this.currentAuthSubPage.set(page);
+  }
 }
