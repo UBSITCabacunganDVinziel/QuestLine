ac1443ff90d79c0d1ae2b0162fbb960d6bcaecc2
barillo_k <barillo_k@f112.ub.lan>
2026-07-02 10:49:22 +0800

    Updated app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestService } from './quest-service';
-import { CharacterHudComponent } from './character-hud/character-hud.component';
-import { QuestFormComponent } from './quest-form/quest-form.component';
-import { QuestListComponent } from './quest-list/quest-list.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { CharacterHud } from './character-hud/character-hud';
+import { QuestForm } from './quest-form/quest-form';
+import { QuestList } from './quest-list/quest-list';
+import { SignIn } from './sign-in/sign-in';
+import { SignUp } from './sign-up/sign-up';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule, 
-    CharacterHudComponent, 
-    QuestFormComponent, 
-    QuestListComponent, 
-    SignInComponent, 
-    SignUpComponent
+    CharacterHud, 
+    QuestForm, 
+    QuestList, 
+    SignIn, 
+    SignUp
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent {
+export class App{
   questService = inject(QuestService);
 
   onDeleteProfile() {
