af7b5e48a371ab1fb587c641f21745ed4b4ef6f9
barillo_k <barillo_k@f112.ub.lan>
2026-07-02 10:33:39 +0800

    Updated sign-up.ts

--- src/app/sign-up/sign-up.ts
+++ src/app/sign-up/sign-up.ts
@@ -1,4 +1,4 @@
-import { Component, inject, output } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { QuestService } from '../quest-service';
@@ -7,24 +7,26 @@ import { QuestService } from '../quest-service';
   selector: 'app-sign-up',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './sign-up.html'
+  template: `
+    <div class="auth-box" *ngIf="!authService.currentUser()">
+      <h3>⚔️ Create Character Profile</h3>
+      <input type="text" [(ngModel)]="username" placeholder="Hero Name" />
+      <input type="password" [(ngModel)]="password" placeholder="Secure Secret Key" />
+      <button (click)="register()">Establish Profile</button>
+      <p style="color: green" *ngIf="msg">{{msg}}</p>
+    </div>
+  `
 })
-export class SignUp {
-  private questService = inject(QuestService);
+export class SignUpComponent {
+  authService = inject(QuestService);
+  username = ''; password = ''; msg = '';
 
-  public username = '';
-  public email = '';
-  public password = '';
-
-  onSwitchToSignIn = output<void>();
-
-  onSignUp() {
-    if (!this.username || !this.email || !this.password) return;
-    this.questService.signUpPlayer({
-      username: this.username,
-      email: this.email,
-      password: this.password
-    });
-    this.onSwitchToSignIn.emit();
+  register() {
+    if(this.authService.signUp(this.username, this.password)) {
+      this.msg = 'Character registered! Proceed to Sign In.';
+      this.username = ''; this.password = '';
+    } else {
+      this.msg = 'Name occupied or invalid!';
+    }
   }
 }
